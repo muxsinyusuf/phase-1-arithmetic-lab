60b11c6b5c522eb4c2788c3a5c3a72c026da81fa
Muxsinyusuf <mucsin.yusuf@student.moringaschool.com>
2023-07-10 22:27:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+
+let set = [10, 5, 20, 15];
+let max = Math.max(...set);
 
 // Write your code here
